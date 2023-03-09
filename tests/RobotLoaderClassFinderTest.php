68f83ca35e8dcb1935aa86cb11f004c1077f31f7
Petr Duda <petrduda@seznam.cz>
2023-03-09 16:38:13 +0100

    deduplicate test methods using data provider

--- tests/RobotLoaderClassFinderTest.php
+++ tests/RobotLoaderClassFinderTest.php
@@ -4,54 +4,65 @@ declare(strict_types = 1);
 
 namespace Consistence\ClassFinder\RobotLoader;
 
+use Generator;
 use Nette\Loaders\RobotLoader;
 use PHPUnit\Framework\Assert;
 
 class RobotLoaderClassFinderTest extends \PHPUnit\Framework\TestCase
 {
 
-	public function testFindClass(): void
+	/**
+	 * @return mixed[][]|\Generator
+	 */
+	public function findByInterfaceDataProvider(): Generator
 	{
-		$classList = array_values($this->getRobotLoaderClassFinder()->findByInterface(ExtendingClass::class));
-
-		Assert::assertContains(ExtendingClass::class, $classList);
-		Assert::assertCount(1, $classList);
-	}
-
-	public function testFindClassAndChildren(): void
-	{
-		$classList = array_values($this->getRobotLoaderClassFinder()->findByInterface(BaseClass::class));
-		sort($classList);
-
-		$expected = [
-			BaseClass::class,
-			ExtendingClass::class,
+		yield 'class' => [
+			'interfaceName' => ExtendingClass::class,
+			'expectedClassList' => [
+				ExtendingClass::class,
+			],
 		];
-
-		Assert::assertEquals($expected, $classList);
-	}
-
-	public function testFindInterface(): void
-	{
-		$classList = array_values($this->getRobotLoaderClassFinder()->findByInterface(NotImplementedInterface::class));
-
-		Assert::assertContains(NotImplementedInterface::class, $classList);
-		Assert::assertCount(1, $classList);
-	}
-
-	public function testFindInterfaceAndImplementations(): void
-	{
-		$classList = array_values($this->getRobotLoaderClassFinder()->findByInterface(BaseInterface::class));
-		sort($classList);
-
-		$expected = [
-			BaseClass::class,
-			BaseInterface::class,
-			ExtendingClass::class,
-			ExtendingInterface::class,
+		yield 'class and children' => [
+			'interfaceName' => BaseClass::class,
+			'expectedClassList' => [
+				BaseClass::class,
+				ExtendingClass::class,
+			],
 		];
+		yield 'not implemented interface' => [
+			'interfaceName' => NotImplementedInterface::class,
+			'expectedClassList' => [
+				NotImplementedInterface::class,
+			],
+		];
+		yield 'interface and implementations' => [
+			'interfaceName' => BaseInterface::class,
+			'expectedClassList' => [
+				BaseClass::class,
+				BaseInterface::class,
+				ExtendingClass::class,
+				ExtendingInterface::class,
+			],
+		];
+	}
 
-		Assert::assertEquals($expected, $classList);
+	/**
+	 * @dataProvider findByInterfaceDataProvider
+	 *
+	 * @param string $interfaceName
+	 * @param string[] $expectedClassList
+	 */
+	public function testFindByInterface(
+		string $interfaceName,
+		array $expectedClassList
+	): void
+	{
+		$classList = array_values($this->getRobotLoaderClassFinder()->findByInterface($interfaceName));
+
+		foreach ($expectedClassList as $expectedClass) {
+			Assert::assertContains($expectedClass, $classList);
+		}
+		Assert::assertCount(count($expectedClassList), $classList);
 	}
 
 	private function getRobotLoaderClassFinder(): RobotLoaderClassFinder
